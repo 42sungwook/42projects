630fb18a38cec4fa491aec3ecbb8499aeabc1faf
Dain Jeong <daijeong@c1r6s3.42seoul.kr>
2023-04-27 16:00:03 +0900

    [Feat] Add 42header

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -1,19 +1,31 @@
-#include "../minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_export.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daijeong <daijeong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 15:59:33 by daijeong          #+#    #+#             */
+/*   Updated: 2023/04/27 15:59:50 by daijeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	print_env(char **envp)
-{
+// #include "../minishell.h"
 
-}
+// int	print_env(char **envp)
+// {
 
-int	add_env(char **envp, char *env)
-{
+// }
 
-}
+// int	add_env(char **envp, char *env)
+// {
 
-int	builtin_export(t_commands *cmd, char **envp)
-{
-	if (cmd[1])
-		return (print_env(envp));
-	else
-		return (add_env(envp, cmd[1]));
-}
+// }
+
+// int	builtin_export(t_commands *cmd, char **envp)
+// {
+// 	if (cmd[1])
+// 		return (print_env(envp));
+// 	else
+// 		return (add_env(envp, cmd[1]));
+// }
