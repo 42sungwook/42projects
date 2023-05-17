073d496647706f2984018844b02b7ece9c313e97
Sungwook Kim <sungwook@c1r6s1.42seoul.kr>
2023-05-17 18:38:31 +0900

    [Fix] echo -n option

--- builtin/builtin_echo.c
+++ builtin/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: sungwook <sungwook@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:59:29 by daijeong          #+#    #+#             */
-/*   Updated: 2023/05/17 14:40:22 by sungwook         ###   ########.fr       */
+/*   Updated: 2023/05/17 18:37:52 by sungwook         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,32 @@
 int	builtin_echo(t_commands *cmds)
 {
 	size_t	i;
+	size_t	j;
+	int		option;
 
 	if (!cmds->cmd[1])
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	if (!ft_strncmp(cmds->cmd[1], "-n", 2))
-		i = 2;
-	else
-		i = 1;
+	i = 1;
+	option = 0;
+	while (cmds->cmd[i])
+	{
+		j = 0;
+		while (cmds->cmd[i][j])
+		{
+			if ((j == 0 && cmds->cmd[i][j] == '-') || \
+				(j != 0 && cmds->cmd[i][j] == 'n'))
+				j++;
+			else
+				break ;
+		}
+		if (cmds->cmd[i][j])
+			break ;
+		option = 1;
+		i++;
+	}
 	if (!cmds->fds->outfile)
 		cmds->fds->outfile = 1;
 	while (cmds->cmd[i])
@@ -34,7 +50,7 @@ int	builtin_echo(t_commands *cmds)
 			write(cmds->fds->outfile, " ", 1);
 		i++;
 	}
-	if (ft_strncmp(cmds->cmd[1], "-n", 2))
+	if (!option)
 		write(cmds->fds->outfile, "\n", 1);
 	return (0);
 }
