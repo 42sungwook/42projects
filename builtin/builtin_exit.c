747d0f9a0f54f0f4b9521176cf449d05d215e42f
Dain Jeong <daijeong@c1r6s2.42seoul.kr>
2023-05-17 17:09:29 +0900

    [Fix] exit exceeding long long range

--- builtin/builtin_exit.c
+++ builtin/builtin_exit.c
@@ -1,39 +1,80 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    .c                                     :+:      :+:    :+:   */
+/*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daijeong <daijeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/27 15:59:31 by daijeong          #+#    #+#             */
-/*   Updated: 2023/05/16 13:18:00 by daijeong         ###   ########.fr       */
+/*   Created: 2023/05/17 13:33:44 by daijeong          #+#    #+#             */
+/*   Updated: 2023/05/17 17:08:04 by daijeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	builtin_exit(t_commands *cmds)
+void	parse_exit_ll(char *str)
 {
-	int		i;
-	int		status;
-	char	*str;
+	long long	result;
+	long long	temp;
+	int			sign;
+	int			i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (!ft_strcmp(str, "-9223372036854775808"))
+		return ;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		str++;
+	if (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign *= -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		temp = result * 10 + (str[i++] - ASCII_ZERO);
+		if (temp < result)
+		{
+			ft_putstr_fd("exit\n", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(str, 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			exit(-1);
+		}
+		result = temp;
+	}
+}
+
+void	print_not_digit(char *str)
+{
+	int	i;
 
 	i = 0;
+	if (str[0] == '-')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i++]))
+		{
+			ft_putstr_fd("exit\n", 2);
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(str, 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			exit(-1);
+		}
+	}
+}
+
+int	builtin_exit(t_commands *cmds)
+{
+	char	*str;
+	int		status;
+
 	status = 0;
 	if (cmds->cmd[1] && cmds->cmd[2])
 	{
 		str = cmds->cmd[1];
-		while (str[i])
-		{
-			if (!ft_isdigit(str[i++]))
-			{
-				ft_putstr_fd("exit\n", 2);
-				ft_putstr_fd("minishell: exit: ", 2);
-				ft_putstr_fd(str, 2);
-				ft_putstr_fd(": numeric argument required\n", 2);
-				exit(-1);
-			}
-		}
+		parse_exit_ll(str);
+		print_not_digit(str);
 		ft_putstr_fd("exit\n", 2);
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		g_exit_status = 1;
@@ -42,17 +83,8 @@ int	builtin_exit(t_commands *cmds)
 	if (cmds->cmd[1])
 	{
 		str = cmds->cmd[1];
-		while (str[i])
-		{
-			if (!ft_isdigit(str[i++]))
-			{
-				ft_putstr_fd("exit\n", 2);
-				ft_putstr_fd("minishell: exit: ", 2);
-				ft_putstr_fd(str, 2);
-				ft_putstr_fd(": numeric argument required\n", 2);
-				exit(-1);
-			}
-		}
+		parse_exit_ll(str);
+		print_not_digit(str);
 		status = ft_atoi(str);
 		ft_putstr_fd("exit\n", 2);
 		exit(status);
