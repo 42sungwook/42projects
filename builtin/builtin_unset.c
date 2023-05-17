48485e0b2d39f478bd08d7f3df5a2274ece3dae6
Sungwook Kim <sungwook@c1r6s1.42seoul.kr>
2023-05-17 20:39:31 +0900

    [Fix] unset builtin

--- builtin/builtin_unset.c
+++ builtin/builtin_unset.c
@@ -6,34 +6,61 @@
 /*   By: sungwook <sungwook@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:59:35 by daijeong          #+#    #+#             */
-/*   Updated: 2023/05/05 20:16:40 by sungwook         ###   ########.fr       */
+/*   Updated: 2023/05/17 20:35:19 by sungwook         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	check_unset_cmd(char *cmd)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(cmd[0]) && cmd[0] != '_')
+	{
+		ft_putstr_fd("minishell: unset: `", 2);
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
+		return (1);
+	}
+	while (cmd[++i])
+	{
+		if (!ft_isalnum(cmd[i]) && cmd[i] != '_')
+		{
+			ft_putstr_fd("minishell: unset: `", 2);
+			ft_putstr_fd(cmd, 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 int	builtin_unset(t_token *token, char **cmd)
 {
-	t_envp	*envp_list;
-	t_envp	*tmp_list;
+	t_envp	*temp;
+	size_t	equal_location;
 	int		i;
 
 	i = 0;
-	envp_list = token->envp;
 	while (cmd[++i])
 	{
-		tmp_list = envp_list;
-		while (tmp_list)
+		temp = token->envp;
+		if (check_unset_cmd(cmd[i]))
+			continue ;
+		while (temp)
 		{
-			if (!ft_strncmp(tmp_list->str, cmd[i], ft_strlen(cmd[i])) && \
-				tmp_list->str[ft_strlen(cmd[i])] == '=')
+			equal_location = cmpcmp(temp->str, '=');
+			if ((equal_location == ft_strlen(cmd[i]) && \
+				!ft_strncmp(temp->str, cmd[i], equal_location)) || \
+				(!equal_location && !ft_strcmp(temp->str, cmd[i])))
 			{
-				envp_list = delete_envp_list(envp_list, tmp_list);
+				token->envp = delete_envp_list(token->envp, temp);
 				break ;
 			}
-			tmp_list = tmp_list->next;
+			temp = temp->next;
 		}
 	}
-	token->envp = envp_list;
 	return (0);
 }
