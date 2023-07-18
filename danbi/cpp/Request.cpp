f21b370bc3d0efd9ccb41df61a4a8d0a635c8068
honeyl3ee <ddanhopark@gmail.com>
2023-07-18 18:04:15 +0900

    [Edit] Request class

--- danbi/cpp/Request.cpp
+++ danbi/cpp/Request.cpp
@@ -1,2 +1,18 @@
 #include "Request.hpp"
 
+Request::Request() : _method(-1), _process(-1) {}
+
+Request::~Request() {}
+
+void Request::parsing(std::string tmp) {
+  _start = int i = 0;
+
+  while (tmp.c_str()[i] && tmp.c_str()[i] != '\r') {
+  }
+}
+
+const std::string Request::getMessage() const {};
+
+enum PROCESS Request::getProcess(){};
+
+enum METHOD Request::getMethod(){};
--- danbi/cpp/Request.hpp
+++ danbi/cpp/Request.hpp
@@ -4,33 +4,25 @@
 #include <iostream>
 #include <map>
 
-enum METHOD {
-  GET,
-  POST,
-  DELETE
-};
+enum METHOD { GET, POST, DELETE };
+enum PROCESS { ERROR, CGI, NORMAL };
 
-enum PROCESS {
-  ERROR,
-  CGI,
-  NORMAL
-};
+class Request {
+ private:
+  std::string _msg;
+  std::map < std::string, int _method;
 
-class Request
-{
-private:
-  std::string msg;
+  int _process;
+  std::map<std::string, std::string> _header;
+  std::string _body;
 
-public:
-	Request();
-	~Request();
-	void  parsing(std::string msg);
+ public:
+  Request();
+  ~Request();
+  void parsing(std::string tmp);
   const std::string getMessage() const;
   enum PROCESS getProcess();
   enum METHOD getMethod();
 };
-
-
-
 
 #endif
