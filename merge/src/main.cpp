9765a4a1356e42cdb59e3aca972e0b132954443a
YJ <soltwood6862@naver.com>
2023-07-24 21:20:55 +0900

    [Test] add ServerInfoList test in main.cpp

--- merge/src/main.cpp
+++ merge/src/main.cpp
@@ -3,26 +3,47 @@
 #include "../includes/RootBlock.hpp"
 #include "../includes/Server.hpp"
 
-RootBlock *config(std::string path) {
+RootBlock *config(std::string path)
+{
   Parser parser(path);
   RootBlock *root = parser.getRootBlock();
   return root;
 }
 
-int main(int ac, char **av) {
-  if (ac != 2) {
+void test(ServerInfo *info)
+{
+  std::cout << "===========INFO===========" << std::endl;
+  std::cout << "_listen: " << info->_listen << std::endl;
+
+  std::list<ServerBlock *>::iterator it;
+  std::cout << std::endl;
+  for (it = info->_serverList.begin(); it != info->_serverList.end(); it++)
+    (*it)->test();
+}
+
+int main(int ac, char **av)
+{
+  if (ac != 2)
+  {
     std::cout << "Invalid Arguments" << std::endl;
     return 1;
   }
   RootBlock *root = config(av[1]);
-  root->test();
+  //  root->test();
+
+  std::list<ServerInfo *> info = root->getServerInfoList();
+
+  std::list<ServerInfo *>::iterator it;
+  std::cout << std::endl;
+  for (it = info.begin(); it != info.end(); it++)
+    test((*it));
 
   /* init server socket and listen */
-  Server server;
-  if (server.init(root->getBlockList()) == EXIT_FAILURE) return EXIT_FAILURE;
+  // Server server;
+  //  if (server.init(root->getBlockList()) == EXIT_FAILURE) return EXIT_FAILURE;
 
   /* main loop */
-  if (server.run() == EXIT_FAILURE) return EXIT_FAILURE;
+  // if (server.run() == EXIT_FAILURE) return EXIT_FAILURE;
 
   return (0);
 }
