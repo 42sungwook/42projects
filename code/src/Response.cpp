e306baeb12f65f08af187f9d2f4c4d2062868966
jaekkang <rkdwornjs123@gmail.com>
2023-07-27 21:36:08 +0900

    [Edit] Response 클래스 수정

--- code/src/Response.cpp
+++ code/src/Response.cpp
@@ -1,24 +1,16 @@
 #include "../includes/Response.hpp"
 
-Response::Response() {}
+Response::Response(std::string result) : _result(result) {}
 
 Response::~Response() {}
 
-void Response::setHeader(std::string key, std::string value) {
-	if (_headers.find(key) == _headers.end())
-		_headers[key] = value;
+void Response::convertCGI(std::string cgiResult) { 
+  // TODO Content-Type만 무조건 해야 함
+  // 따라서 status line이나 Content-Length는 없으면 우리가 추가
+  _result = cgiResult; 
 }
 
-void Response::setBody(std::string body) {
-	_body = body;
-}
-
-const std::string Response::getResponse() const {
-	std::string response = "HTTP/1.1 200 OK\n";
-	std::map<std::string, std::string>::const_iterator it;
-	for (it = _headers.begin(); it != _headers.end(); it++)
-		response += it->first + ": " + it->second + "\n";
-	if (!_body.empty())
-		response += "\n" + _body;
-	return response;
+int Response::sendResponse(int clientSocket) {
+  if (write(clientSocket, _result.c_str(), _result.length()) == -1) return (1);
+  return (0);
 }
