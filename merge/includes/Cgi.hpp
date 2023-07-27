b8eefdcbf4b6f3981991a3dc7d262519d7e1fb75
김성욱 <sungwook@MacAir-2.local>
2023-07-27 13:54:38 +0900

    [ADD} Cgi files from jaekkang and juyojeon

---
+++ merge/includes/Cgi.hpp
@@ -0,0 +1,31 @@
+#ifndef CGI_HPP
+#define CGI_HPP
+
+#include <unistd.h>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+class Cgi {
+ private:
+  char** _envp;
+  std::string _res; // cgi가 보내주는 response
+  std::map<std::string, std::string> _env;
+  std::string _cgiPath;
+
+  // header parsing data를 받아서 _env 생성
+  void makeEnv(std::map<std::string, std::string> param);
+
+ public:
+  Cgi();
+  ~Cgi();
+
+  // client's request를 받아서 execve에 사용할 _envp를 생성
+  void reqToEnvp(std::map<std::string, std::string> param);
+  // _envp, body(parsing)를 받아서 cgi를 실행 
+  void excute(std::string body);
+  std::string& getRet();
+};
+
+#endif
