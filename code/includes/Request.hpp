b07eeac4095f2698a5ff9f9efc56ed81d7ed9e24
Sungwook Kim <sungwook@c3r6s1.42seoul.kr>
2023-07-31 20:24:03 +0900

    [Fix] uneeded functions

--- code/includes/Request.hpp
+++ code/includes/Request.hpp
@@ -16,22 +16,26 @@ class Request {
   std::map<std::string, std::string> _header;
   std::string _body;
   std::string _host;
+  std::string _autoindex;
   enum MIME _mime;
   int _port;
   int _status;
 
  private:
-  void parseUrl();
+  const int parseUrl();
 
  public:
   Request();
   ~Request();
   void parsing(const std::string &raw);
+  void setAutoindex(std::string &value);
   const std::string getHost();
   const int &getPort();
   const std::string getUri();
   const std::string getMessage() const;
   const int &getStatus() const;
+	const std::string &getAutoindex() const;
+	enum MIME &getMime() const;
   enum PROCESS getProcess();
   enum METHOD getMethod();
 };
