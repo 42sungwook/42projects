4ec1122fed6db7ecbc4028bbe00f9df2f5c4c659
honeyl3ee <ddanhopark@gmail.com>
2023-08-03 20:16:51 +0900

    [Edit] ServerOperator and Request

--- code/includes/Request.hpp
+++ code/includes/Request.hpp
@@ -3,18 +3,21 @@
 
 #include <sys/stat.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <map>
 #include <sstream>
+#include <string>
 
 #include "RootBlock.hpp"
 
 enum METHOD { GET, POST, DELETE };
 enum PROCESS { CGI, NORMAL };
-enum MIME { DIRECTORY, HTML, CSS, JS, JPG, PNG, GIF, TXT, PDF, JSON };
+enum MIME { DIRECTORY, HTML, CSS, JS, JPG, PNG, GIF, TXT, PDF, JSON, OCTET };
 
 class Request {
  private:
+  std::string _rawContents;
   std::map<std::string, std::string> _header;
   std::string _body;
   std::string _host;
@@ -22,6 +25,7 @@ class Request {
   enum MIME _mime;
   int _port;
   int _status;
+  bool _isFullReq;
 
  private:
   void parseUrl();
@@ -29,17 +33,25 @@ class Request {
  public:
   Request();
   ~Request();
-  void parsing(const std::string &raw);
+  void parsing();
   void setAutoindex(std::string &value);
+  void clear();
+  void addRawContents(const std::string &raw);
+  void addHeader(std::string key, std::string value);
   const std::string getHost();
   const int &getPort();
   const std::string getUri();
   const std::string getMessage() const;
+  const std::string getBody() const;
   const int &getStatus() const;
   const std::string &getAutoindex() const;
-  enum MIME &getMime() const;
+  enum MIME getMime() const;
   enum PROCESS getProcess();
   std::string getMethod();
+  bool isFullReq() const;
+
+  // temp
+  std::string getRawContents() const;
 };
 
 #endif
--- code/includes/ServerOperator.hpp
+++ code/includes/ServerOperator.hpp
@@ -3,21 +3,20 @@
 #include <list>
 
 #include "Kqueue.hpp"
+#include "Request.hpp"
 #include "Server.hpp"
 
 class ServerOperator {
  private:
   ServerMap &_serverMap;
   LocationMap &_locationMap;
-  std::map<int, std::string> _clients;
-  std::map<int, int> _clientToServer; // key: client socket, value: serversocket
+  std::map<int, Request> _clients;
+  std::map<int, int>
+      _clientToServer;  // key: client socket, value: serversocket
 
-  void addClientContents(int clientSock, std::string buffer);
-  void setClientContentsClear(int clientSock);
   void disconnectClient(int clientSock);
-  const std::string getClientContents(int clientSock);
   bool isExistClient(int clientSock);
-  RootBlock *getLocationBlock(Request &req, ServerBlock *sb);
+  ServerBlock *getLocationBlock(Request &req, ServerBlock *sb);
 
   void handleEventError(struct kevent *event);
   void handleReadEvent(struct kevent *event, Kqueue kq);
